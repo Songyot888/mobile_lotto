8139d24315504872d4ab156d41bd5cd97ccecee1
Songyot888 <dog1235you@gmail.com>
2025-09-05 19:41:15 +0700

    d

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_lotto/page/login_page.dart';
-import 'package:mobile_lotto/page/menu_page.dart';
-import 'package:mobile_lotto/page/register_page.dart';
-import 'package:mobile_lotto/page/register_page.dart';
-import 'package:mobile_lotto/page/wallet_page.dart';
+import 'package:mobile_lotto/page/load_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +18,7 @@ class MyApp extends StatelessWidget {
           seedColor: const Color.fromARGB(255, 77, 1, 255),
         ),
       ),
-      home: Login_Page(),
+      home: Load_Page(),
     );
   }
 }
---
+++ lib/page/load_page.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:mobile_lotto/page/login_page.dart';
+
+class Load_Page extends StatefulWidget {
+  const Load_Page({super.key});
+
+  @override
+  State<Load_Page> createState() => _Load_PageState();
+}
+
+class _Load_PageState extends State<Load_Page> {
+  Color maroon = const Color(0xFF017E89);
+
+  @override
+  void initState() {
+    super.initState();
+    // Timer(const Duration(seconds: 3), () {
+    //   Navigator.pushReplacement(
+    //     context,
+    //     MaterialPageRoute(builder: (context) => const Login_Page()),
+    //   );
+    // });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: maroon,
+      body: Stack(
+        children: [
+          // โลโก้ + สโลแกนกลางจอ
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // โลโก้คำว่า Lotto888
+                Image.asset('assets/lotto888.png', width: 500, height: 200),
+                const Text(
+                  'จ่ายหนัก จ่ายจริง ไม่จำกัด',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ],
+            ),
+          ),
+
+          // ข้อความกำลังโหลด... ด้านล่าง
+          const Align(
+            alignment: Alignment(0, 0.85),
+            child: Text(
+              'กำลังโหลด...',
+              style: TextStyle(color: Colors.white70, fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
